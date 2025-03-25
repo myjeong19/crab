6a88ea0ec24740b4de95e0df994f9cc4361b8d5b
myjeong19 <myjeong19@naver.com>
2025-03-26 05:59:57 +0900

    feat. function

---
+++ src/funtion.rs
@@ -0,0 +1,7 @@
+pub fn function (qty:f64,oz:f64)->f64 {
+    // tail expression
+    // return qty * oz;
+    qty * oz
+}
+
+// * Rust function does not support variable numbers of arguments or different types
